5e3793d0746d9a56616fe40ea77895f46b7540f6
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-05-17 17:28:06 +0530

    added dummy api call for testing retrofit setup

--- app/src/main/java/com/example/rohantaneja/hv_placesautocomplete/ui/MainActivity.java
+++ app/src/main/java/com/example/rohantaneja/hv_placesautocomplete/ui/MainActivity.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 
 import com.example.rohantaneja.hv_placesautocomplete.R;
+import com.example.rohantaneja.hv_placesautocomplete.network.RetrofitAdapter;
+import com.example.rohantaneja.hv_placesautocomplete.network.response.PlaceBaseResponse;
+import com.example.rohantaneja.hv_placesautocomplete.util.Constants;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+    public static final String TAG = MainActivity.class.getName();
 
     private EditText mSearchEditText;
     private RecyclerView mSuggestionsRecyclerView;
@@ -27,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         mSearchEditText.addTextChangedListener(this);
 
         mSuggestionsRecyclerView = findViewById(R.id.suggestions_recycler_view);
+
+        getPlacesFromApi();
     }
 
     @Override
@@ -42,5 +54,36 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
     @Override
     public void afterTextChanged(Editable s) {
 
+    }
+
+    private void getPlacesFromApi() {
+        RetrofitAdapter retrofitAdapter = new RetrofitAdapter(Constants.BASE_URL);
+        Call<PlaceBaseResponse> apiCall = retrofitAdapter.getPlacesApi().getPlaceSuggestions(
+                Constants.RESULTS_FORMAT.JSON,
+                "delhi",
+                Constants.API_KEY
+        );
+
+        apiCall.enqueue(new Callback<PlaceBaseResponse>() {
+            @Override
+            public void onResponse(Call<PlaceBaseResponse> call, Response<PlaceBaseResponse> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        Log.i(TAG, response.body().getPlaceDetails().get(0).getName());
+                        Log.i(TAG, response.body().getPlaceDetails().get(0).getPlaceId());
+                        Log.i(TAG, response.body().getPlaceDetails().get(0).getIcon());
+                    } else {
+                        Log.i(TAG, "respose.body() is null: " + response.message());
+                    }
+                } else {
+                    Log.i(TAG, "respose is null");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PlaceBaseResponse> call, Throwable t) {
+                Log.i(TAG, "onFail(): " + t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rohantaneja/hv_placesautocomplete/util/Constants.java
+++ app/src/main/java/com/example/rohantaneja/hv_placesautocomplete/util/Constants.java
@@ -5,4 +5,9 @@ public class Constants {
     public static final String API_KEY = "AIzaSyA4Okotu4gYuZW9zJaOWfDqjAgUEwRcNqQ";
 
     public static final String BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch";
+
+    public interface RESULTS_FORMAT {
+        String JSON = "json";
+        String XML = "xml";
+    }
 }
