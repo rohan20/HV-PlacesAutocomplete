8ae06dcd17893407680c70a4f7508349d599148a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-05-17 16:41:11 +0530

    added suitable packages for modularisation of code

--- app/src/main/java/com/example/rohantaneja/hv_placesautocomplete/ui/MainActivity.java
+++ app/src/main/java/com/example/rohantaneja/hv_placesautocomplete/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.rohantaneja.hv_placesautocomplete;
+package com.example.rohantaneja.hv_placesautocomplete.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.rohantaneja.hv_placesautocomplete.R;
 
 public class MainActivity extends AppCompatActivity {
 
